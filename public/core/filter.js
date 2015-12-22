4b2d99b0e55fcfbf5386262697f497bd1a1f9d0f
Aurelien <aurelien.negrier@epitech.eu>
2015-12-22 03:15:33 +0100

    remove error filter backline

--- public/core/filter.js
+++ public/core/filter.js
@@ -1,5 +1,7 @@
 app.filter('backline', function () {
   return function (item) {
-    return item.replace(/\n/g, "<br />");
+  	if (item) {
+  		return item.replace(/\n/g, "<br />");
+  	}
   };
 });
